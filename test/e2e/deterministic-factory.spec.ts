2f04540e50d8ad89bf3e3a6c5ea2db47ba83680e
Ged <0xged@protonmail.com>
2022-09-21 16:38:52 -0300

    test: fix e2e to use mean deployer

--- test/e2e/deterministic-factory.spec.ts
+++ test/e2e/deterministic-factory.spec.ts
@@ -1,21 +1,27 @@
 import { TransactionResponse } from '@ethersproject/abstract-provider';
 import { BigNumber, constants, utils } from 'ethers';
-import { deployments, ethers } from 'hardhat';
+import { deployments, ethers, getNamedAccounts } from 'hardhat';
 import { evm } from '@utils';
 import { then, when } from '@utils/bdd';
 import { DeterministicFactory, ERC20Mock, ERC20Mock__factory } from '@typechained';
 import { expect } from 'chai';
 import { randomHex } from 'web3-utils';
 import { getCreationCode } from '@utils/contracts';
+import { JsonRpcSigner } from '@ethersproject/providers';
+import { impersonate, setBalance } from '@utils/wallet';
 
 // This has all been tested in solmate's repo, but I don't want to be left out
 describe('DeterministicFactory', () => {
   let deterministicFactoryContract: DeterministicFactory;
   let snapshotId: string;
+  let meanDeployer: JsonRpcSigner;
 
   before(async () => {
     await deployments.run(['DeterministicFactory']);
     deterministicFactoryContract = await ethers.getContract<DeterministicFactory>('DeterministicFactory');
+    const namedAccounts = await getNamedAccounts();
+    meanDeployer = await impersonate(namedAccounts.meanDeployer);
+    await setBalance({ account: namedAccounts.meanDeployer, balance: constants.MaxUint256 });
     snapshotId = await evm.snapshot.take();
   });
 
@@ -26,7 +32,7 @@ describe('DeterministicFactory', () => {
   when('we deploy on a normal workflow', () => {
     then('deploys and gets contract correctly', async () => {
       const ERC20DeploymentSalt = randomHex(32);
-      await deterministicFactoryContract.deploy(
+      await deterministicFactoryContract.connect(meanDeployer).deploy(
         ERC20DeploymentSalt,
         getCreationCode({
           bytecode: ERC20Mock__factory.bytecode,
