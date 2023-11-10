8f5f2b76110e6d8defcff8e5e1bb0f862c3d78cc
Ged <0xged@protonmail.com>
2023-11-10 14:50:35 -0300

    feat: adds custom address to utils

--- utils/deployment.ts
+++ utils/deployment.ts
@@ -5,6 +5,11 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { hexlify, ParamType } from 'ethers/lib/utils';
 import { ArtifactData, DeployResult } from '@0xged/hardhat-deploy/types';
 
+const DEFAULT_DETERMINISTIC_FACTORY_ADDRESS = '0xD420ea5a1981dB5f1914954CE6e012A3bB10c015';
+const DETERMINISTIC_FACTORY_ADDRESS: { [chainId: number]: string } = {
+  30: '0xc11154859da6854E1C2A0826a45abe4E6A2eD46A',
+};
+
 export const getCreationCode = ({
   bytecode,
   constructorArgs,
@@ -44,6 +49,8 @@ export const deployThroughDeterministicFactory = async ({
     deployerSigner = await ethers.getSigner(deployer);
   }
 
+  const chainId = (await deployerSigner.provider!.getNetwork()).chainId;
+
   const existingDeployment = await hre.deployments.getOrNull(name);
   if (!!existingDeployment) {
     if (skipIfAlreadyDeployed === undefined || skipIfAlreadyDeployed === null || skipIfAlreadyDeployed) {
@@ -56,7 +63,10 @@ export const deployThroughDeterministicFactory = async ({
     }
   }
 
-  const deterministicFactory = await ethers.getContractAt(DETERMINISTIC_FACTORY_ABI, '0xD420ea5a1981dB5f1914954CE6e012A3bB10c015');
+  const deterministicFactory = await ethers.getContractAt(
+    DETERMINISTIC_FACTORY_ABI,
+    DETERMINISTIC_FACTORY_ADDRESS[chainId] || DEFAULT_DETERMINISTIC_FACTORY_ADDRESS
+  );
 
   const saltToUse = process.env.USE_RANDOM_SALT === 'true' ? hexlify(utils.randomBytes(10)) : salt;
   const saltAsBytes = utils.formatBytes32String(saltToUse);
